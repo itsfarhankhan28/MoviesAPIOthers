5d3f29e851124a2314eca0a29853faacc0b84ae6
itsfarhankhan28 <famannat@gmail.com>
2023-09-27 23:57:18 +0530

    feat(server): changed route file path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const PORT = 4000
-const fileupload = require('./routes/route')
+const fileupload = require('./api/route')
 const {cloudinaryConfig} = require('./config/cloudinaryconfig')
 
 app.use(express.json())
